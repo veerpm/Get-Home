e5f911a3eed546e2455bbedde633f25e38f5841e
gdumouli <gdumouli@ualberta.ca>
2023-09-30 18:43:17 -0600

    "Pause Game" Feature Player can pause the game by pressing "p" (code is subject to expand with further updates)

---
+++ Assets/Get Home/Scripts/GamePause.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePause : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject pauseScreen;
+
+    private bool gamePaused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            gamePaused = !gamePaused;
+            player.GetComponent<PlayerMovement>().enabled = !gamePaused;
+            pauseScreen.SetActive(gamePaused);
+        }
+    }
+}
