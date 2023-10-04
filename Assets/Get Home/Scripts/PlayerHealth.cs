38a07011079729530d5462a061ed9c3d38690502
PhuNguyen16 <phunguyen2816@gmail.com>
2023-10-04 13:21:18 -0600

    Enemy follows you, enemy can shoot stuff at you, enemy stops moving when they are shooting you

--- Assets/Get Home/Scripts/PlayerHealth.cs
+++ Assets/Get Home/Scripts/PlayerHealth.cs
@@ -26,7 +26,7 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
-    void TakeDamage(int damage)
+    public void TakeDamage(int damage)
     {
         currentHealth -= damage;
         DisplayHealth();
