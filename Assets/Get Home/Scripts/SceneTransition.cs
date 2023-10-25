134ddfb7d93bdb6c198792048433b2937e3a2d75
gdumouli <gdumouli@ualberta.ca>
2023-10-24 21:49:30 -0600

    Main menu finished

---
+++ Assets/Get Home/Scripts/SceneTransition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransition : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    // setup sound before 1st frame
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("Volume"))
+        {
+            audioSource.volume = PlayerPrefs.GetFloat("Volume");
+        }
+    }
+}
