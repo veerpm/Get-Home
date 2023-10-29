45407c2b6ef3aa73b1f031bfab28ed89f4c0362c
Veer Paras Mehra <vmehra@ualberta.ca>
2023-10-28 21:49:28 -0600

    changed animation speed and range for football, working on goImage

---
+++ Assets/Get Home/Scripts/GoPrompt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float blinkInterval = 1.0f;
+    private float nextToggleTime;
+
+    private LockFrame lockFrame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // lockFrame = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
