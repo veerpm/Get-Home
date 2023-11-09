457c4a74d55f43a423b50fab56651026e5aa7e41
Veer Paras Mehra <vmehra@ualberta.ca>
2023-11-09 11:55:15 -0700

    changed logic for spraycan to use collider

--- Assets/Get Home/Scripts/SprayCanBehaviour.cs
+++ Assets/Get Home/Scripts/SprayCanBehaviour.cs
@@ -6,12 +6,13 @@ using UnityEngine;
 public class SprayCanBehaviour : MonoBehaviour
 {
     public GameObject player;
+    public LayerMask enemyLayers;
     public float dist = 3f;
     public float height = 2f;
     public int damage = 5;
     public int attackRate = 1;
     private float nextAttackTime = 0f;
-    public Transform attackPoint;
+    public GameObject attackPoint;
 
     // Start is called before the first frame update
     void Start()
@@ -24,42 +25,43 @@ public class SprayCanBehaviour : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.E) && Time.time >= nextAttackTime)
         {
-            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
-            foreach (GameObject enemy in enemies)
+            Collider2D area = attackPoint.GetComponent<Collider2D>();
+            List<Collider2D> enemies = new List<Collider2D>();
+            ContactFilter2D filter = new ContactFilter2D();
+            filter.SetLayerMask(enemyLayers);
+            Physics2D.OverlapCollider(area, filter, enemies);
+            foreach (Collider2D enemy in enemies)
             {
-                if (WithinTriangle(enemy.transform.position))
-                {
-                    enemy.GetComponent<EnemyHealth>().TakeDamage(damage);
-                }
+                enemy.GetComponent<EnemyHealth>().TakeDamage(damage);
             }
             nextAttackTime = Time.time + 1f / attackRate;
         }
 
     }
 
-    /* Returns true if point p lies inside triangle a-b-c */
-    bool WithinTriangle(Vector3 enemyPos)
-    {
-        Vector2 v0 = new Vector2 (dist, height/2);
-        Vector2 v1 = new Vector2 (dist, -height/2);
-        Vector2 v2 = enemyPos - attackPoint.position;
-        float dot00 = Vector2.Dot(v0, v0);
-        float dot01 = Vector2.Dot(v0, v1);
-        float dot02 = Vector2.Dot(v0, v2);
-        float dot11 = Vector2.Dot(v1, v1);
-        float dot12 = Vector2.Dot(v1, v2);
-        float invDenom = 1.0f / (dot00 * dot11 - dot01 * dot01);
-        float u = (dot11 * dot02 - dot01 * dot12) * invDenom;
-        float v = (dot00 * dot12 - dot01 * dot02) * invDenom;
-        return (u > 0.0f) && (v > 0.0f) && (u + v < 1.0f);
-    }
+    ///* Returns true if point p lies inside triangle a-b-c */
+    //bool WithinTriangle(Vector3 enemyPos)
+    //{
+    //    Vector2 v0 = new Vector2 (dist, height/2);
+    //    Vector2 v1 = new Vector2 (dist, -height/2);
+    //    Vector2 v2 = enemyPos - attackPoint.transform.position;
+    //    float dot00 = Vector2.Dot(v0, v0);
+    //    float dot01 = Vector2.Dot(v0, v1);
+    //    float dot02 = Vector2.Dot(v0, v2);
+    //    float dot11 = Vector2.Dot(v1, v1);
+    //    float dot12 = Vector2.Dot(v1, v2);
+    //    float invDenom = 1.0f / (dot00 * dot11 - dot01 * dot01);
+    //    float u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+    //    float v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+    //    return (u > 0.0f) && (v > 0.0f) && (u + v < 1.0f);
+    //}
 
-    private void OnDrawGizmos()
-    {
-        //Transform attackPoint = player.transform.Find("SprayCanAttackPoint");
-        Vector3 pos1 = new Vector3(attackPoint.position.x + dist, attackPoint.position.y + height / 2, attackPoint.position.z);
-        Vector3 pos2 = new Vector3(attackPoint.position.x + dist, attackPoint.position.y - height / 2, attackPoint.position.z);
-        Gizmos.DrawLine(attackPoint.position, pos1);
-        Gizmos.DrawLine(attackPoint.position, pos2);
-    }
+    //private void OnDrawGizmos()
+    //{
+    //    //Transform attackPoint = player.transform.Find("SprayCanAttackPoint");
+    //    Vector3 pos1 = new Vector3(attackPoint.position.x + dist, attackPoint.position.y + height / 2, attackPoint.position.z);
+    //    Vector3 pos2 = new Vector3(attackPoint.position.x + dist, attackPoint.position.y - height / 2, attackPoint.position.z);
+    //    Gizmos.DrawLine(attackPoint.position, pos1);
+    //    Gizmos.DrawLine(attackPoint.position, pos2);
+    //}
 }
