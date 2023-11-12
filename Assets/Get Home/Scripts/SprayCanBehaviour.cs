1fd1d592c4c591e99e3ffe9d136d313a85e814a4
Veer Paras Mehra <vmehra@ualberta.ca>
2023-11-11 19:31:10 -0700

    added glow from weapons when allowed to pickup and created seperate gameobject for spray collider

--- Assets/Get Home/Scripts/SprayCanBehaviour.cs
+++ Assets/Get Home/Scripts/SprayCanBehaviour.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class SprayCanBehaviour : MonoBehaviour
 {
-    public GameObject player;
     public LayerMask enemyLayers;
     public float dist = 3f;
     public float height = 2f;
@@ -14,24 +13,36 @@ public class SprayCanBehaviour : MonoBehaviour
     private float nextAttackTime = 0f;
     public GameObject attackPoint;
     public ParticleSystem ps;
+    public GameObject area;
+    private List<Collider2D> enemies = new List<Collider2D>();
+    ContactFilter2D filter = new ContactFilter2D();
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        ps = attackPoint.transform.GetChild(0).GetComponent<ParticleSystem>();
+        filter.SetLayerMask(enemyLayers);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (GetComponent<PlayerMovement>().lookingRight && area.transform.localScale.x < 0)
+        {
+            var shape = ps.shape;
+            shape.rotation = new Vector3(0, 90, 0);
+            area.transform.localScale = new Vector2(-area.transform.localScale.x, area.transform.localScale.y);
+        }
+        else if (!GetComponent<PlayerMovement>().lookingRight && area.transform.localScale.x > 0)
+        {
+            var shape = ps.shape;
+            shape.rotation = new Vector3(0, -90, 0);
+            area.transform.localScale = new Vector2(-area.transform.localScale.x, area.transform.localScale.y);
+        }
+
+        area.transform.position = attackPoint.transform.position;
         if (Input.GetKey(KeyCode.E) && Time.time >= nextAttackTime)
         {
-            Collider2D area = attackPoint.GetComponent<Collider2D>();
-            List<Collider2D> enemies = new List<Collider2D>();
-            ContactFilter2D filter = new ContactFilter2D();
-            filter.SetLayerMask(enemyLayers);
-            Physics2D.OverlapCollider(area, filter, enemies);
+            Physics2D.OverlapCollider(area.GetComponent<Collider2D>(), filter, enemies);
             foreach (Collider2D enemy in enemies)
             {
                 enemy.GetComponent<EnemyHealth>().TakeDamage(damage);
@@ -60,30 +71,4 @@ public class SprayCanBehaviour : MonoBehaviour
 
 
     }
-
-    ///* Returns true if point p lies inside triangle a-b-c */
-    //bool WithinTriangle(Vector3 enemyPos)
-    //{
-    //    Vector2 v0 = new Vector2 (dist, height/2);
-    //    Vector2 v1 = new Vector2 (dist, -height/2);
-    //    Vector2 v2 = enemyPos - attackPoint.transform.position;
-    //    float dot00 = Vector2.Dot(v0, v0);
-    //    float dot01 = Vector2.Dot(v0, v1);
-    //    float dot02 = Vector2.Dot(v0, v2);
-    //    float dot11 = Vector2.Dot(v1, v1);
-    //    float dot12 = Vector2.Dot(v1, v2);
-    //    float invDenom = 1.0f / (dot00 * dot11 - dot01 * dot01);
-    //    float u = (dot11 * dot02 - dot01 * dot12) * invDenom;
-    //    float v = (dot00 * dot12 - dot01 * dot02) * invDenom;
-    //    return (u > 0.0f) && (v > 0.0f) && (u + v < 1.0f);
-    //}
-
-    //private void OnDrawGizmos()
-    //{
-    //    //Transform attackPoint = player.transform.Find("SprayCanAttackPoint");
-    //    Vector3 pos1 = new Vector3(attackPoint.position.x + dist, attackPoint.position.y + height / 2, attackPoint.position.z);
-    //    Vector3 pos2 = new Vector3(attackPoint.position.x + dist, attackPoint.position.y - height / 2, attackPoint.position.z);
-    //    Gizmos.DrawLine(attackPoint.position, pos1);
-    //    Gizmos.DrawLine(attackPoint.position, pos2);
-    //}
 }
