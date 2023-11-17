5caced8bb07d8d18d106a9166fb7a80e6e30cb25
PhuNguyen16 <phunguyen2816@gmail.com>
2023-11-16 17:21:51 -0700

    Added bg art for boss level, Note: Level with boss is called level_boss. Also, changed camera size for boss level to be 5

---
+++ Assets/Get Home/Scripts/BossBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBehaviour : MonoBehaviour
+{
+    private Transform player;
+    public float speed = 2;
+    public float lineofSight;
+    public float throwingRange;
+    private float nextThrowTime;
+    public float throwingCD;
+    public GameObject throwable;
+    public GameObject throwSpot;
+    private bool lookingRight = false;
+    private Transform canvas;
+    public Animator animator;
+    public AudioSource throwSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        canvas = transform.Find("Canvas");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.x > transform.position.x && !lookingRight)
+        {
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+            canvas.localScale = new Vector2(-canvas.localScale.x, canvas.localScale.y);
+            lookingRight = true;
+        }
+        // Player to the left of the enemy
+        else if (player.transform.position.x < this.transform.position.x && lookingRight)
+        {
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+            canvas.localScale = new Vector2(-canvas.localScale.x, canvas.localScale.y);
+            lookingRight = false;
+        }
+    }
+
+    void ThrowingStance()
+    {
+        // Throwing while planted!
+    }
+}
