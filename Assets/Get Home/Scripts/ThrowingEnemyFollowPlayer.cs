358ee56ab3f13eed81dd04ba5542678ec32683db
PhuNguyen16 <phunguyen2816@gmail.com>
2023-11-21 19:13:15 -0700

    Increased football enemies speed so they can retreat better, increased their damage per shot, increased damage for baseball enemy as well

--- Assets/Get Home/Scripts/ThrowingEnemyFollowPlayer.cs
+++ Assets/Get Home/Scripts/ThrowingEnemyFollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ThrowingEnemyFollowPlayer : MonoBehaviour
 {
     public Transform player;
-    private float speed = 2;
+    private float speed = 4;
     public float lineofSight = 5.0f;
     public float throwingRange;
     public float escapeRange;
@@ -60,7 +60,7 @@ public class ThrowingEnemyFollowPlayer : MonoBehaviour
             animator.SetBool("Walking", true);
         }
         // Throwly Poly
-        else if (distanceFromPlayer < throwingRange && nextThrowTime < Time.time)
+        else if (escapeRange < distanceFromPlayer && distanceFromPlayer < throwingRange && nextThrowTime < Time.time)
         {
             animator.SetTrigger("ThrowAttack");
             //sound FX
@@ -69,6 +69,13 @@ public class ThrowingEnemyFollowPlayer : MonoBehaviour
             Instantiate(throwable, throwSpot.transform.position, Quaternion.identity);
             nextThrowTime = Time.time + throwingCD;
             animator.SetBool("Walking", false);
+        }
+        // Skaddadle
+        else if (distanceFromPlayer < escapeRange)
+        {
+            Debug.Log("THIS CODE EXISTS!" + escapeVector);
+            transform.position = Vector2.MoveTowards(this.transform.position, this.transform.position + escapeVector, (speed /2) * Time.deltaTime);
+            animator.SetBool("Walking", true);
         }
 
     }
