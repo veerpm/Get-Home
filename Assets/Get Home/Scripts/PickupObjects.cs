d8b0ff4e407710849d0f2af9c89ff20993ebfdd5
PhuNguyen16 <phunguyen2816@gmail.com>
2023-11-28 21:31:34 -0700

    Changed pickup button for trash can to be R, trash cans can only be thrown now

--- Assets/Get Home/Scripts/PickupObjects.cs
+++ Assets/Get Home/Scripts/PickupObjects.cs
@@ -9,12 +9,12 @@ public class PickupObjects : MonoBehaviour
     public LayerMask pickUpMask;
     public float pickupRadius = 0.9f;
 
-    public Vector3 Direction {get; set;}
+    public Vector3 Direction { get; set; }
     private GameObject itemHolding;
     private Collider2D pickUpItem;
     Collider2D item;
     public float placeDownOffSet = 0f;
-    private Vector3 yOffset = new Vector3 (0.0f, -0.3f, 0.0f);
+    private Vector3 yOffset = new Vector3(0.0f, -0.3f, 0.0f);
 
     //sound FX
     public AudioSource pickUpTrashSound;
@@ -41,74 +41,67 @@ public class PickupObjects : MonoBehaviour
         }
 
         //Debug.Log(Direction);
-        if (Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.R))
         {
-
             if (itemHolding)
             {
-                if (Mathf.Abs(Direction.x) > 0)
-                {
-                    //sound FX
-                    throwTrashSound.Play();
-                    gameObject.GetComponent<PlayerMovement>().speed = 3;
-                    itemHolding.transform.parent = null;
-                    itemHolding.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-                    itemHolding.GetComponent<Rigidbody2D>().constraints &= ~RigidbodyConstraints2D.FreezePositionX;
-                    if (itemHolding.CompareTag("Trash Can"))
-                    {
-                        itemHolding.GetComponent<ThrownObjectsHitDetect>().thrown = true;
-                        //itemHolding.transform.Find("Light").GetComponent<Light2D>().enabled = false;
-                        //item = null;
-                        Debug.Log("Sausage McMuffin");
-                    }
-                  
-                    if (itemHolding.CompareTag("Football"))
-                    {
-                        itemHolding.GetComponent<Rigidbody2D>().AddForce(Direction * 600);
-                    }
-                    itemHolding.GetComponent<Rigidbody2D>().AddForce(Direction * 200);
-                    GetComponent<PlayerCombatMelee>().enabled = true;
+                //sound FX
+                throwTrashSound.Play();
+                gameObject.GetComponent<PlayerMovement>().speed = 3;
+                itemHolding.transform.parent = null;
+                itemHolding.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                itemHolding.GetComponent<Rigidbody2D>().constraints &= ~RigidbodyConstraints2D.FreezePositionX;
 
-                    if (itemHolding.GetComponent<Rigidbody2D>())
-                        itemHolding.GetComponent<Rigidbody2D>().simulated = true;
-                    Destroy(itemHolding, 0.5f);
-                    itemHolding = null;
-                }
-                else
+                if (itemHolding.CompareTag("Trash Can"))
                 {
-                    //sound FX
-                    pickUpTrashSound.Play();
+                    itemHolding.GetComponent<ThrownObjectsHitDetect>().thrown = true;
 
                     if (GetComponent<PlayerMovement>().lookingRight == true)
                     {
-                        itemHolding.transform.position = new Vector3(holdSpot.position.x, holdSpot.position.y - placeDownOffSet, 0.0f) + new Vector3(0.6f, 0.3f, 0);
-                        GetComponent<PlayerCombatMelee>().enabled = true;
-                        itemHolding.transform.parent = null;
-                        if (itemHolding.GetComponent<Rigidbody2D>())
-                            itemHolding.GetComponent<Rigidbody2D>().simulated = true;
-                        itemHolding = null;
+                        itemHolding.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 200);
                     }
                     else
                     {
-                        itemHolding.transform.position = new Vector3(holdSpot.position.x, holdSpot.position.y - placeDownOffSet, 0.0f) + new Vector3(-0.6f, 0.3f, 0);
-                        GetComponent<PlayerCombatMelee>().enabled = true;
-                        itemHolding.transform.parent = null;
-                        if (itemHolding.GetComponent<Rigidbody2D>())
-                            itemHolding.GetComponent<Rigidbody2D>().simulated = true;
-                        itemHolding = null;
+                        itemHolding.GetComponent<Rigidbody2D>().AddForce(Vector2.left * 200);
                     }
-                  
+
+                    Debug.Log("Sausage McMuffin");
                 }
-                
+
+                if (itemHolding.CompareTag("Football"))
+                {
+                    if (GetComponent<PlayerMovement>().lookingRight == true)
+                    {
+                        itemHolding.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 200);
+                    }
+                    else
+                    {
+                        itemHolding.GetComponent<Rigidbody2D>().AddForce(Vector2.left * 200);
+                    }
+                }
+
+                GetComponent<PlayerCombatMelee>().enabled = true;
+                if (itemHolding.GetComponent<Rigidbody2D>())
+                    itemHolding.GetComponent<Rigidbody2D>().simulated = true;
+
+                itemHolding.transform.parent = null;
+                // Automatically destroy thrown object after 0.5 seconds
+                Destroy(itemHolding, 0.5f);
+                itemHolding = null;
             }
+
             else
             {
                 if (pickUpItem)
                 {
+                    //sound FX
+                    pickUpTrashSound.Play();
                     gameObject.GetComponent<PlayerMovement>().speed = 1.5f;
                     itemHolding = pickUpItem.gameObject;
                     itemHolding.transform.position = holdSpot.position;
                     itemHolding.transform.parent = transform;
+
+                    // Disable attacking when carrying object
                     GetComponent<PlayerCombatMelee>().enabled = false;
                     if (itemHolding.CompareTag("Football"))
                     {
