21c3a54ede1993756a2f35a1cb2650901a8deb60
gdumouli <90409961+gdumouli@users.noreply.github.com>
2023-12-09 14:46:04 -0700

    Boss Intro There is now a camera shake when the boss enters and a delay before the dialogue. The delay of the dialogue can be tweaked in the script parameters of LandlordClone

--- Assets/Get Home/Scripts/LandlordClone.cs
+++ Assets/Get Home/Scripts/LandlordClone.cs
@@ -5,10 +5,28 @@ using UnityEngine;
 public class LandlordClone : MonoBehaviour
 {
     public GameObject gameManager;
+    public GameObject gameCamera;
+    public float delayDialogue;
 
     // called at the end of the boss' entry animation
     public void DialogueStart()
     {
+        StartCoroutine(DelayedDialogue());
+    }
+
+    public void StartCameraShake()
+    {
+        gameCamera.GetComponent<CameraMovement>().shake = true;
+    }
+
+    public void StopCameraShake()
+    {
+        gameCamera.GetComponent<CameraMovement>().shake = false;
+    }
+
+    IEnumerator DelayedDialogue()
+    {
+        yield return new WaitForSeconds(delayDialogue);
         gameManager.GetComponent<BossEpilogue>().StartIntroDialogue();
     }
 }
